f4608ffa28e87e6e0ac1acb15bb3cc33b1669970
AnSpake <AnSpake@proton.me>
2026-08-04 19:10:50 +0200

    ytb-top: Argparse

--- youtube-top-history.py
+++ youtube-top-history.py
@@ -13,6 +13,21 @@ def parse_args():
                         help="Path to your watch_history file")
     parser.add_argument('--top', required=False, type=int, default=10,
                         help="Number of top videos for the scoreboard")
+    parser.add_argument('--today', required=False,
+                        help="Get the scoreboard of the current month")
+
+    # FIXME: default = current month, current year
+    parser.add_argument('--month', required=False,
+                        help="Get the scoreboard of the given month")
+    parser.add_argument('--year', required=False,
+                        help="Get the scoreboard of the given year")
+
+    parser.add_argument('--all-month', required=False,
+                        help="Get the scoreboard for each month of current/given year")
+    parser.add_argument('--all-year', required=False,
+                        help="Get the scoreboard for each years")
+    parser.add_argument('--all', required=False,
+                        help="Get the scoreboard for each years + each months of every years")
 
     args = parser.parse_args()
     return args
